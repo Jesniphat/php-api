f610bf75a843f01a10cf3b7e823ed9afff67bf07
Jesse <jesniphat@hotmail.com>
2019-08-10 07:54:48 +0700

    move basecontroller to service

--- api/api.php
+++ api/api.php
@@ -11,7 +11,7 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
  * Inclode base file
  */
 include_once './vendor/autoload.php';
-include_once './controllers/baseController.php';
+include_once './services/baseController.php';
 include_once './services/routing.php';
 include_once './services/permission.php';
 
